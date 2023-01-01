ddd7c2f1d6dd696eb7c97f7138d11ae60d658351
Anthony Stender <anthony.m.stender@gmail.com>
2023-01-01 09:07:00 -0600

    Updated Monthly Stats area

--- src/MonthlyStats.js
+++ src/MonthlyStats.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Table } from "react-bootstrap";
 
 function MonthlyStats(props) {
+
+  const d = new Date();
+  let currentMonth = d.getMonth();
+
   return (
     <div className='Stats'>
       <h3 className='text-center'>
@@ -21,47 +25,47 @@ function MonthlyStats(props) {
           </tr>
           <tr>
             <td>February</td>
-            <td>{props.distances.februaryDistance.toFixed(2)}</td>
+            <td>{currentMonth >= 1 ? props.distances.februaryDistance.toFixed(2) : 'TBD'}</td>
           </tr>
           <tr>
             <td>March</td>
-            <td>{props.distances.marchDistance.toFixed(2)}</td>
+            <td>{currentMonth >= 2 ? props.distances.marchDistance.toFixed(2) : 'TBD'}</td>
           </tr>
           <tr>
             <td>April</td>
-            <td>{props.distances.aprilDistance.toFixed(2)}</td>
+            <td>{currentMonth >= 3 ? props.distances.aprilDistance.toFixed(2) : 'TBD'}</td>
           </tr>
           <tr>
             <td>May</td>
-            <td>{props.distances.mayDistance.toFixed(2)}</td>
+            <td>{currentMonth >= 4 ? props.distances.mayDistance.toFixed(2) : 'TBD'}</td>
           </tr>
           <tr>
             <td>June</td>
-            <td>{props.distances.juneDistance.toFixed(2)}</td>
+            <td>{currentMonth >= 5 ? props.distances.juneDistance.toFixed(2) : 'TBD'}</td>
           </tr>
           <tr>
             <td>July</td>
-            <td>{props.distances.julyDistance.toFixed(2)}</td>
+            <td>{currentMonth >= 6 ? props.distances.julyDistance.toFixed(2) : 'TBD'}</td>
           </tr>
           <tr>
             <td>August</td>
-            <td>{props.distances.augustDistance.toFixed(2)}</td>
+            <td>{currentMonth >= 7 ? props.distances.augustDistance.toFixed(2) : 'TBD'}</td>
           </tr>
           <tr>
             <td>September</td>
-            <td>{props.distances.septemberDistance.toFixed(2)}</td>
+            <td>{currentMonth >= 8 ? props.distances.septemberDistance.toFixed(2) : 'TBD'}</td>
           </tr>
           <tr>
             <td>October</td>
-            <td>{props.distances.octoberDistance.toFixed(2)}</td>
+            <td>{currentMonth >= 9 ? props.distances.octoberDistance.toFixed(2) : 'TBD'}</td>
           </tr>
           <tr>
             <td>November</td>
-            <td>{props.distances.novemberDistance.toFixed(2)}</td>
+            <td>{currentMonth >= 10 ? props.distances.novemberDistance.toFixed(2) : 'TBD'}</td>
           </tr>
           <tr>
             <td>December</td>
-            <td>{props.distances.decemberDistance.toFixed(2)}</td>
+            <td>{currentMonth >= 11 ? props.distances.decemberDistance.toFixed(2) : 'TBD'}</td>
           </tr>
         </tbody>
       </Table>
